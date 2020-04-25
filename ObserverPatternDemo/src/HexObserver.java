7873f992eb3141220bab98ed294550ec06e42ce5
fakker19 <50959985+fakker19@users.noreply.github.com>
2020-04-25 10:57:28 -0500

    Interface

---
+++ ObserverPatternDemo/src/HexObserver.java
@@ -0,0 +1,13 @@
+public class HexObserver implements Observer {
+
+    private Subject subject;
+    public HexObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Hex String: " + Integer.toHexString(subject.getState()));
+    }
+}
---
+++ ObserverPatternDemo/src/ObserverPatternDemo.java
@@ -0,0 +1,12 @@
+public class ObserverPatternDemo {
+    public static void main(String[]args){
+        Subject subject = new Subject();
+
+        new BinaryObserver(subject);
+        new OctalObserver(subject);
+        new HexObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+    }
+}
